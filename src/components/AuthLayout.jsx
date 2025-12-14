e1ed200768e2500850af1cf9d9de3c12c3ba16b0
ilhamhadziq <ilhamhadziq24@gmail.com>
2025-12-15 00:11:02 +0700

    update: login & register

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,15 +1,15 @@
 // src/components/AuthLayout.jsx
-import "../assets/css/header.css";
+import "../assets/css/general.css";
 export default function AuthLayout({ children }) {
   return (
-    <>
-      <header>
+    <div>
+      <header className="side">
         <img src="src/assets/images/logo.png" className="logo" />
       </header>
 
       <main>
         <section className="login-card">{children}</section>
       </main>
-    </>
+    </div>
   );
 }
